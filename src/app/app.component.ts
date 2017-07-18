929574d13f11f51c0e96b63b2439dcb7bc9b1332
ArtiGaur <arti.gaur@ewebcore.net>
2017-07-18 14:57:26 +0530

    profile pages styling done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,89 +7,89 @@ import { StorageProvider } from '../providers/storage/storage';
 // import {HomePage} from "../pages/home/home"
 import { AngularFireAuth } from 'angularfire2/auth';
 // import {Transfer} from "@ionic-native/transfer"
-import{Camera} from "@ionic-native/camera"
+import { Camera } from "@ionic-native/camera"
 import { Storage } from '@ionic/storage';
 // import { OneSignal } from '@ionic-native/onesignal';
-  @Component({
+@Component({
   templateUrl: 'app.html'
-  })
-  export class MyApp {
-  rootPage:any;
+})
+export class MyApp {
+  rootPage: any;
 
   @ViewChild(Nav) nav: Nav;
-  pages: Array<{title: string, component: any}>;
+  pages: Array<{ title: string, component: any }>;
   userProfile
-  daycare:boolean;
+  daycare: boolean;
   domainUrl
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,private menuCtrl: MenuController,private afAuth: AngularFireAuth,public auth:AuthProvider
-  ,public storage:StorageProvider,public store:Storage, public event:Events
-  // , public oneSignal:OneSignal
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private menuCtrl: MenuController, private afAuth: AngularFireAuth, public auth: AuthProvider
+    , public storage: StorageProvider, public store: Storage, public event: Events
+    // , public oneSignal:OneSignal
   ) {
-      platform.ready().then(() => {
+    platform.ready().then(() => {
 
-          statusBar.styleDefault();
-          splashScreen.hide();
-     
-         
-    
-      });
-      const authObserver = afAuth.authState.subscribe( user => {
-        this.domainUrl =this.auth.domainStorageUrl
-        if (user) {
-      //      this.oneSignal.startInit('f1edd1a5-35f5-4b50-bbdc-ccc8bcdfd420', '398522364586');
+      statusBar.styleDefault();
+      splashScreen.hide();
 
-      //   this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
-      //   // alert(user.uid)
-      //   this.oneSignal.sendTags({user_id: user.uid});
-      //   this.oneSignal.handleNotificationReceived().subscribe((data) => {
-      //   // do something when notification is received
-      //   console.log(JSON.stringify(data))
-      //   });
 
-      //   this.oneSignal.handleNotificationOpened().subscribe(() => {
-      //     // do something when a notification is opened
-      //   });
-      // this.oneSignal.endInit();
-              this.rootPage = "HomePage";
 
-              this.event.subscribe('userProfile', (userProfile) => {
-              this.userProfile = userProfile;
-              // console.log("from appcomponent ===>>>> "+this.userProfile)
-              this.storage.getStorage("isparent").then(data=>{
-                
-              if(data){
-                this.daycare=false
-              }else if(!data){
-                this.daycare=true
-              }
-              })
-         
-            });
+    });
+    const authObserver = afAuth.authState.subscribe(user => {
+      this.domainUrl = this.auth.domainStorageUrl
+      if (user) {
+        //      this.oneSignal.startInit('f1edd1a5-35f5-4b50-bbdc-ccc8bcdfd420', '398522364586');
 
-            authObserver.unsubscribe();
+        //   this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
+        //   // alert(user.uid)
+        //   this.oneSignal.sendTags({user_id: user.uid});
+        //   this.oneSignal.handleNotificationReceived().subscribe((data) => {
+        //   // do something when notification is received
+        //   console.log(JSON.stringify(data))
+        //   });
 
-        } else {
-            // this.rootPage = 'SelectDaycarePage';
-            this.rootPage = 'LoginPage';
-            this.event.subscribe('userProfile', (userProfile) => {
-              this.userProfile = userProfile;
-              this.storage.getStorage("isparent").then(data=>{
-              if(data){
-                this.daycare=false
-              }else if(!data){
-                this.daycare=true
-              }
-              })
-              // console.log("from appcomponent ===>>>> "+this.userProfile)
-            })
-            authObserver.unsubscribe();
-        }
-      })
+        //   this.oneSignal.handleNotificationOpened().subscribe(() => {
+        //     // do something when a notification is opened
+        //   });
+        // this.oneSignal.endInit();
+        this.rootPage = "HomePage";
+
+        this.event.subscribe('userProfile', (userProfile) => {
+          this.userProfile = userProfile;
+          // console.log("from appcomponent ===>>>> "+this.userProfile)
+          this.storage.getStorage("isparent").then(data => {
+
+            if (data) {
+              this.daycare = false
+            } else if (!data) {
+              this.daycare = true
+            }
+          })
+
+        });
+
+        authObserver.unsubscribe();
+
+      } else {
+        // this.rootPage = 'SelectDaycarePage';
+        this.rootPage = 'LoginPage';
+        this.event.subscribe('userProfile', (userProfile) => {
+          this.userProfile = userProfile;
+          this.storage.getStorage("isparent").then(data => {
+            if (data) {
+              this.daycare = false
+            } else if (!data) {
+              this.daycare = true
+            }
+          })
+          // console.log("from appcomponent ===>>>> "+this.userProfile)
+        })
+        authObserver.unsubscribe();
+      }
+    })
 
 
   }
-  
-  onLogout(){ 
+
+  onLogout() {
     // this.storage.removeStorage("isparent")
     this.store.remove("isparent")
     this.menuCtrl.close();
@@ -99,10 +99,10 @@ import { Storage } from '@ionic/storage';
 
   }
 
-  onLoad(page: any){
+  onLoad(page: any) {
     this.nav.setRoot(page);
     this.menuCtrl.close();
-  } 
-
   }
+
+}
 
